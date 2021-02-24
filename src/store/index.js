a7404c3117dd62263d57f925942a20b0fae41f3f
ricraAay <riicray@gamil.com>
2021-02-24 16:44:29 +0300

    Создана структура проекта

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import registration from './modules/registration'
+import authorization from './modules/authorization'
+import payment from './modules/payment'
+import passwordRecovery from './modules/passwordRecovery'
+import products from './modules/products'
+import ordering from './modules/ordering'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -11,5 +18,11 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    registration,
+    authorization,
+    payment,
+    passwordRecovery,
+    products,
+    ordering
   }
 })
